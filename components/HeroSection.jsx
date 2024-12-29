667b21ac224ec9e0cab443878434f898beb93d5d
mayanksinghal-dev <mayanksinghal.dev@gmail.com>
2024-12-30 03:07:13 +0530

    issues resolved

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -12,13 +12,13 @@ const HeroSection = ({contactRef}) => {
     };
     return (
         <section className="relative flex flex-col items-center justify-between py-4 lg:py-12 ">
-            <Image
+            {/* <Image
                 src='/hero.svg'
                 alt="hero"
                 width={1572}
                 height={795}
                 className="absolute -top-[98px] -z-10"
-            />
+            /> */}
             <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
                 <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
                     <h1 className="text-3xl font-bold leading-10 md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
