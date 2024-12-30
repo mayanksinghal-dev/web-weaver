35e188fc6f5e6b89988418f5963d82c4c7416365
mayanksinghal-dev <mayanksinghal.dev@gmail.com>
2024-12-30 14:21:36 +0530

    adjusted width

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <main className="min-h-screen relative px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem]">
+      <body>
+        <main className="min-h-screen relative px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[84.3rem] 2xl:max-w-[91.7rem] 4k:max-w-full">
           <Navbar />
           {children}
           <Footer />
