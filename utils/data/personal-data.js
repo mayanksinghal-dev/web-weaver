765c6ffd96c29117bbb0f901745fa714638f7aa3
mayanksinghal-dev <mayanksinghal.dev@gmail.com>
2026-01-14 06:06:55 +0530

    more exp

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Mayank Singhal",
   profile: '/profile.png',
   designation: "Senior Software Developer",
-  description : "Results-oriented Full stack Engineer with 3 years of experience delivering scalable full-stack solutions, secure APIs, and distributed microservices for enterprise applications. Proven expertise in Java, Spring Boot, Kafka, Redis, and ReactJS with strong grounding in Agile, DevOps and AWS practices. Recognized with Accenture’s Skills Star Award for driving innovation, boosting developer productivity, and leading high-impact system designs under tight deadlines. Adept at requirement analysis, system architecture, and end-to-end delivery from concept to production.",
+  description : "Results-oriented Full stack Engineer with 3+ years of experience delivering scalable full-stack solutions, secure APIs, and distributed microservices for enterprise applications. Proven expertise in Java, Spring Boot, Kafka, Redis, and ReactJS with strong grounding in Agile, DevOps and AWS practices. Recognized with Accenture’s Skills Star Award for driving innovation, boosting developer productivity, and leading high-impact system designs under tight deadlines. Adept at requirement analysis, system architecture, and end-to-end delivery from concept to production.",
   email: 'mayanksinghal.dev@gmail.com',
   phone: '+91 7986197130',
   address: 'Gurgaon, Harayana - 122015',
